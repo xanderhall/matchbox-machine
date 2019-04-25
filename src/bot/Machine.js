dc22913ce05171609ff425eb430386d45762ee0b
Matthew Greenslade <mattgr2010@gmail.com>
2019-04-25 18:50:22 -0230

    reworked Machine.js to remove move resolution from render

--- src/bot/Machine.js
+++ src/bot/Machine.js
@@ -1,59 +1,57 @@
-import React from 'react'
-import PropTypes from 'prop-types'
 import sample from 'lodash/sample';
+import React from 'react';
+import Board from '../components/Board'
 
-export default class MatchboxMachine extends React.PureComponent {
+export default class MatchboxMachine extends React.Component {
   constructor(props) {
     super(props);
+    this.props.getMachineEvaluationFunction(this.resolveMove.bind(this));
     this.state = {
       stateMap: new Map(),
       history: [],
     }
   }
 
-  static propTypes = {
-    gameState: PropTypes.object.isRequired,
-    registerMove: PropTypes.func.isRequired,
-    shouldRegisterMove: PropTypes.bool,
-    legalMoves: PropTypes.array,
-    endCondition: PropTypes.string
-  }
-
-  resolveMove() {
-    const state = this.props.gameState;
-    const stateString = JSON.stringify(state);
+  resolveMove(props) {
+    if (props.endCondition) {
+      this.resolveEnd(props.endCondition);
+      return;
+    }
+    const state = props.state;
+    const key = JSON.stringify(state);
     const map = this.state.stateMap;
-    const history = this.state.history.slice(0, this.props.turn + 1);
+    const history = this.state.history.slice(0, props.turn + 1);
 
     // Check if current game state has previously been evaluated
-    if (!map.has(stateString)) {
+    if (!map.has(key)) {
       const weightMap = [];
-      this.props.legalMoves.forEach((move) => weightMap[move] = 1);
-      map.set(stateString, weightMap);
+      props.legalMoves.forEach((move) => weightMap[move] = 1);
+      map.set(key, weightMap);
     }
 
     // Get list of available moves that haven't been 'marked off'
-    const availableMoves = map.get(stateString);
+    const availableMoves = map.get(key);
+
     const usefulMoves = [];
     availableMoves.forEach((weight, move) => {
       if (weight > 0) usefulMoves.push(move);
     })
     const randomMove = sample(usefulMoves);
 
-    // Register the move with the game
-    this.props.registerMove(randomMove);
-
-    // Store the moves for this game
-    history[this.props.turn] = { state: state, move: randomMove };
+    // Store the move for this game
+    history[props.turn] = { state: state, move: randomMove };
     this.setState({
       history: history,
     });
+
+    // Register the move with the game
+    props.registerMove(randomMove);
   }
 
-  resolveEnd() {
-    if (this.props.endCondition === 'X') {
+  resolveEnd(type) {
+    if (type === 'X') {
       this.resolveLoss();
-    } else if (this.props.endCondition === 'O') {
+    } else if (type === 'O') {
       this.resolveWin();
     } else {
       this.resolveTie();
@@ -72,10 +70,10 @@ export default class MatchboxMachine extends React.PureComponent {
     // Set weight of last move used to 0
     const lastMove = this.state.history.slice().pop();
     const map = this.state.stateMap;
-    const stateString = JSON.stringify(lastMove.state);
-    const availableMoves = map.get(stateString).slice();
+    const key = JSON.stringify(lastMove.state);
+    const availableMoves = map.get(key).slice();
     availableMoves[lastMove.move] = 0;
-    map.set(stateString, availableMoves);
+    map.set(key, availableMoves);
     if (availableMoves.every(weight => !weight)) {
       this.resolveLoss();
     }
@@ -83,28 +81,42 @@ export default class MatchboxMachine extends React.PureComponent {
 
   renderMatchboxes() {
     const rows = [];
-    this.state.stateMap.forEach((moves, stateString) => {
+    this.state.stateMap.forEach((moves, key) => {
       rows.push(
-        <tr key={stateString}>
-          <td>For the state given by {stateString}, the bot has these weights:</td>
-          {moves.map((weight, move) => <td key={move}>{move}: {weight}</td>)}
+        <tr key={key}>
+          <td><Board squares={JSON.parse(key)}/></td><td>{this.renderWeightTable(moves)}</td>
         </tr>
       );
     });
     return rows;
   }
 
-  render() {
-    if (this.props.endCondition) {
-      this.resolveEnd();
-    } else if (this.props.shouldRegisterMove) {
-      this.resolveMove();
-    }
+  renderWeightTable(moves) {
+    const headers = [];
+    const bodies = [];
+    moves.forEach((weight, move) => {
+      headers.push(
+        <th key={move}>({Math.floor(move/3)},{move%3})</th>
+      );
+      bodies.push(
+        <td key={move} class={weight ? 'alert alert-success' : 'alert alert-danger'}>{weight}</td>
+      )
+    });
+    return (
+      <table class='table table-bordered text-center'>
+        <thead class='thead-light'><tr>{headers}</tr></thead>
+        <tbody><tr>{bodies}</tr></tbody>
+      </table>
+    )
+  }
 
+  render() {
     return (
       <div className='machine'>
         <div className='machine-info'>
           <h1>Hi, I'm a machine!</h1>
+          As you play, I'll get better at this game.
+          Below, you can see the choices I'm making each time you make a move.
         </div>
         <table className='matchboxes'>
           {this.renderMatchboxes()}
