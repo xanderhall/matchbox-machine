d7095253ce1730dd465f0d92495bba3e131accc8
Matthew Greenslade <mattgr2010@gmail.com>
2019-04-25 18:53:30 -0230

    Keeping only the restart game button until the machine can work properly with stepping between moves

--- src/components/InfoPanel.js
+++ src/components/InfoPanel.js
@@ -2,20 +2,18 @@ import React from 'react';
 import StepButton from './StepButton';
 
 export default function InfoPanel(props) {
-  const moveList = props.history.map((step, i) =>
-    <StepButton
-      key={i}
-      step={i}
-      jumpTo={() => props.jumpTo(i)}
-    />
-  );
+  // const moveList = props.history.map((step, i) =>
+  //   <StepButton
+  //     key={i}
+  //     step={i}
+  //     jumpTo={() => props.jumpTo(i)}
+  //   />
+  // );
 
   return (
     <div className="game-info">
       <div>{props.status}</div>
-      <ol>
-        <StepButton step={0} jumpTo={() => props.jumpTo(0)}></StepButton>
-      </ol>
+      <StepButton step={0} jumpTo={props.jumpTo}></StepButton>
     </div>
   );
 }
