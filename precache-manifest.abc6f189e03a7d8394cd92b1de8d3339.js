dec864a60952af43924f0acf9e5ae56b73b9b5b7
Matthew Greenslade <mattgr2010@gmail.com>
2019-04-25 01:07:34 -0230

    Updates

---
+++ precache-manifest.abc6f189e03a7d8394cd92b1de8d3339.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "64061432fe4b8856a9bd",
+    "url": "/matchbox-machine/static/css/main.77c0d737.chunk.css"
+  },
+  {
+    "revision": "64061432fe4b8856a9bd",
+    "url": "/matchbox-machine/static/js/main.d1b9e11a.chunk.js"
+  },
+  {
+    "revision": "9650f8ab81e70be00e99",
+    "url": "/matchbox-machine/static/js/runtime~main.4bc065b5.js"
+  },
+  {
+    "revision": "42782adac370571df135",
+    "url": "/matchbox-machine/static/js/2.7e3c310b.chunk.js"
+  },
+  {
+    "revision": "553626e81fb356f107ed0c99c43e44a9",
+    "url": "/matchbox-machine/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/xanderhall/matchbox-machine/precache-manifest.74eec78eae117794104f002c85ed6241.js"
+  "/matchbox-machine/precache-manifest.abc6f189e03a7d8394cd92b1de8d3339.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/xanderhall/matchbox-machine/index.html", {
+workbox.routing.registerNavigationRoute("/matchbox-machine/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
