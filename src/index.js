ada073366c4b79ac69aaa7f2cdc2a8c8a5f65108
Matthew Greenslade <mattgr2010@gmail.com>
2019-04-25 18:49:22 -0230

    added bootstrap css

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Game from './components'
 // ========================================
-
 ReactDOM.render(
   <Game />,
   document.getElementById('root')
