46ed354ad2992ec774a551edf40bd473a0989360
Matthew Greenslade <mattgr2010@gmail.com>
2019-04-25 18:52:36 -0230

    added noop default for onClick so that boards can be displayed but not clickable

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import Square from './Square';
 import { chunk } from 'lodash';
+import PropTypes from 'prop-types';
 
 export default class Board extends React.Component {
+
+  static propTypes = {
+    squares: PropTypes.array.isRequired,
+    onClick: PropTypes.func
+  }
+
+  static defaultProps = {
+    onClick: () => {},
+  }
+  
   renderSquare(i) {
     return (
       <Square
@@ -15,9 +26,7 @@ export default class Board extends React.Component {
 
   render() {
     const squares = this.props.squares.map((square, i) => this.renderSquare(i));
-    const rows = chunk(squares, 3).map((row, i) =>
-      <tr key={i} className="board-row">{row}</tr>
-    );
+    const rows = chunk(squares, 3).map((row, i) => <tr key={i} className="board-row">{row}</tr>);
 
     return (
       <table className="game-board">
