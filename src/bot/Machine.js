eff396462d35477adc609d5c4dd7aa5d160bc214
Matthew Greenslade <mattgr2010@gmail.com>
2019-04-26 18:25:22 -0230

    -Reverted code for resolveLoss. Machine now simply recursively sets to 0 IFF all weights for that state are 0

--- src/bot/Machine.js
+++ src/bot/Machine.js
@@ -33,7 +33,8 @@ export default class MatchboxMachine extends React.Component {
     const highestWeight = moves.reduce((a, b) => Math.max(a, b));
 
     const usefulMoves = Object.keys(moves).filter(move => moves[move] === highestWeight);
-    const randomMove = sample(usefulMoves);
+    //const randomMove = sample(usefulMoves);
+    const randomMove = usefulMoves[0];
 
     // Store the move for this game
     this.setState({
@@ -77,13 +78,12 @@ export default class MatchboxMachine extends React.Component {
     const map = {...this.state.decisionMap};
     const history = this.state.history.slice();
     let lastMove, key;
-    let newWeight = 0;
+    
     do {
       lastMove = history.pop();
       key = JSON.stringify(lastMove.state);
-      map[key][lastMove.move] = newWeight;
-      newWeight = 1;
-    } while (map[key].every(weight => weight === 0));
+      map[key][lastMove.move] = 0;
+    } while (map[key].every(weight => !weight));
 
     this.setState({
       decisionMap: map
@@ -98,6 +98,9 @@ export default class MatchboxMachine extends React.Component {
           As you play, I'll get better at this game.
           Below, you can see the choices I'm making each time you make a move.
         </div>
+        <button class='btn btn-secondary' onClick={() => this.setState({decisionMap: {}})}>
+          Reset bot
+        </button>
         <StateTable states={this.state.decisionMap} />
       </div>
     )
