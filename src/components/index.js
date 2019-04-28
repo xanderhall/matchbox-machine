5a8134b8c6d0e3e4dec8a6c88c8a82a326506a72
Matthew Greenslade <mattgr2010@gmail.com>
2019-04-28 19:06:50 -0230

    Added all components to index for easier importing

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,19 @@
 import Game from './Game'
+import InfoPanel from './info/InfoPanel'
+import StepButton from './info/StepButton'
+import Machine from './bot/Machine'
+import StateTable from './bot/StateTable'
+import Board from './board/Board';
+import Square from './board/Square';
+import WeightTable from './bot/WeightTable';
 
-export default Game;
+export {
+  Game,
+  InfoPanel,
+  StepButton,
+  Machine,
+  StateTable,
+  WeightTable,
+  Board,
+  Square
+};
